65cb846e282398bfef32aa7c46be0ff974cdf88e
Patrick Schuster <schusterpa@stud.sbg.ac.at>
2016-10-14 00:39:12 +0200

    add test program which can be compiled to our binary

---
+++ test_program.c
@@ -0,0 +1,20 @@
+int c;
+
+int main(){
+	int i;
+	i = 0;
+	while(i < 10){
+		//print((int*) "test");
+		print(1);	// immer undefined?! wie print importieren????
+		i = i + 1;
+	}
+	return 1;
+}
+//int main(int argc, char *argv[]){
+//
+//	int i;
+//	for(i = 0; i < 10; i++){
+//		printf("%d \n", i);
+//	}
+//	return 0;
+//}
