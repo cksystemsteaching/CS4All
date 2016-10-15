c7bf6f3c21762293d1de1fad98d5fd07bca0cb93
christian <mueller110@gmx.net>
2016-10-15 09:59:02 +0200

    sample.c file (link with selfie.c for print)

---
+++ sample.c
@@ -0,0 +1,10 @@
+int main(int argc, int *argv){
+	int *out;
+	int mipsterID;
+	initLibrary();
+	hypsterID=hypster_ID();
+	print("Executing: ");
+	printInteger(hypsterID);
+	println();
+	return 0;
+}
