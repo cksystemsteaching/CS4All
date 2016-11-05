830f54aba5a158f08c4544ce84f05beef2a4c797
alindl <andreas.lindlbauer@stud.sbg.ac.at>
2016-11-05 18:34:04 +0100

    New tests provided by sarming

---
+++ verify/a1-inc-prefix/10-deref-exp.c
@@ -0,0 +1,17 @@
+
+// enabled: true
+// expected: 42
+// assemblyOk: 41
+// assemblyNg: 66
+int main(int argc, int* argv) {
+  int* i;
+  //initLibrary();
+  i = malloc(8); // 2x int
+  *(i+1)=41; // msg
+  if( ++*(i+1) == 42)// msg
+    return *(i+1); // msg
+  else
+    return 43;
+
+  return *(i+1);
+}
---
+++ verify/a1-inc-prefix/12-exp-deref.c
@@ -0,0 +1,12 @@
+
+// enabled: true
+// expected: 42
+// assemblyOk: 41
+// assemblyNg: 66
+int main(int argc, int* argv) {
+  int* i;
+  i = malloc(8); // 2x int
+  *i=0; // msg
+  *(i+1)=41;
+  return ++*(++i);// msg
+}
