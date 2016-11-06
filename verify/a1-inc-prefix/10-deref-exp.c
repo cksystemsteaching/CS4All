16c65214da53b2dc91e8ffa8a65dae28251f9f88
Bernhard Neuhauser <bneuhaus@cosy.sbg.ac.at>
2016-11-06 12:34:53 +0100

    removed dead code

--- verify/a1-inc-prefix/10-deref-exp.c
+++ verify/a1-inc-prefix/10-deref-exp.c
@@ -8,9 +8,7 @@ int main(int argc, int* argv) {
   i = malloc(8); // 2x int
   *(i+1)=41; // msg
   if ( ++*(i+1) == 42) // msg
-   return *(i+1); // msg
+    return *(i+1); // msg
   else
     return 43;
-
-  return *(i+1);
 }
