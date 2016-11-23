ad5d645a39be686c20a6466de16f4ad0d4c7bf1c
sprengerst <stefansprenger@gmx.at>
2016-11-23 12:00:25 +0100

    updated testfiles shm

--- testfiles/shmexample.c
+++ testfiles/shmexample.c
@@ -11,12 +11,15 @@ int main(int argc, int *argv){
 	vaddr = shm_map(0,id);
 	//hypsterID = hypster_ID();
 
-	*(vaddr+4100) = 12345;
+	//*(vaddr) = 0;
 
-	*(vaddr+15000) = 12345;
+	*(vaddr+1) = *(vaddr+1) + 1;
 
 	printd("SHMEXAMPLE ID",id);
 	printd("SHMEXAMPLE size",size);
-	printd("SHMEXAMPLE vaddr",vaddr);
+
+	printd("SHMEXAMPLE vaddr",*(vaddr+1));
+
+	//printd("SHMEXAMPLE vaddrsegfault",*(vaddr+17000));
 	return 0;
 }
