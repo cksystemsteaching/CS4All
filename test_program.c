d0a5f79bb73d88e2a43f43771298a262bac8b7f3
Alex Gru <grusch.alex@gmail.com>
2016-11-23 23:07:20 +0100

    print state of variables in test_program before writing

--- test_program.c
+++ test_program.c
@@ -18,13 +18,23 @@ int main(){
 		fd_a = shm_open("a");
 		fd_b = shm_open("b");
 
+		a = shm_read(fd_a);
+		b = shm_read(fd_b);
+
+		print((int*) "[BEFORE WRITE] a: ");
+		printInteger(a);
+		print((int*) ", b: ");
+		printInteger(b);
+		println();
+
 		shm_write(fd_a, i);
 		a = shm_read(fd_a);
 
 		shm_write(fd_b, squared);
 		b = shm_read(fd_b);
 
-		print((int*) "a: ");
+
+		print((int*) "[AFTER WRITE] a: ");
 		printInteger(a);
 		print((int*) ", b: ");
 		printInteger(b);
